01776df122161c8d1290efe476311b98934217e1
shooterfei <shooterfei@163.com>
2022-06-09 18:00:35 +0800

    doomemacs config init

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(menu-bar-mode nil)
+ '(package-selected-packages
+   '(atom-one-dark-theme ivy leaf pandoc-mode pandoc neotree ##))
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
